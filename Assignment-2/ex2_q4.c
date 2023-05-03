4f9d8e9ef6fcc305c19231f09e06345aa079fbe5
yotam4h <66330334+yotam4h@users.noreply.github.com>
2023-05-03 15:52:57 +0000

    a2-q4 is hell

--- Assignment-2/ex2_q4.c
+++ Assignment-2/ex2_q4.c
@@ -16,6 +16,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 #define scanf_s scanf
 // #define PATH1 "c:\\temp\\file1.txt"
 // #define PATH2 "c:\\temp\\file2.txt"
@@ -30,7 +33,7 @@
 
 int memoryReport(char *filename);
 int flenLongestLine(FILE *fd);
-
+int parseline(char const *line);
 // --------------------------------------- //
 // Main section:
 // --------------------------------------- //
@@ -42,15 +45,15 @@ int main()
 	printf("Start Program\n");
 
 	// call functions:
-	total1 = memoryReport(PATH1);
+	// total1 = memoryReport(PATH1);
 	total2 = memoryReport(PATH2);
-	total3 = memoryReport(PATH3);
+	// total3 = memoryReport(PATH3);
 
 	// write output:
 	printf("Output:\n");
-	printf("First file required %d bytes\n", total1);
+	// printf("First file required %d bytes\n", total1);
 	printf("Second file required %d bytes\n", total2);
-	printf("Third file required %d bytes\n", total3);
+	// printf("Third file required %d bytes\n", total3);
 
 	return 0;
 }
@@ -65,12 +68,11 @@ int main()
 int memoryReport(char *filename)
 {
 	// your code:
-	// data types: char, short, int, unsigned int, long, long long, float, double
-	// calcVar: dataType*sizeof(dataType)*howMany
 
 	FILE *fd = NULL;
+	int totalsize = 0, buffersize, typesize[] = {4, 8, 8, 4, 2, 1, 4, 4};
 	char *buffer = NULL;
-	int buffersize;
+
 	// TODO: edgecases
 
 	fd = fopen(filename, "r");
@@ -80,21 +82,30 @@ int memoryReport(char *filename)
 	buffer = (char *)malloc(buffersize * sizeof(char));
 	// TODO: if NULL
 
-	fgets(buffer, buffersize, fd);
-	fputs(buffer,stdout);
-
 	// read line
-	// memory report line
+	fgets(buffer, buffersize, fd);
+	while (!feof(fd))
+	{
+		// parse line
+		totalsize += parseline(buffer);
+		// read next line
+		fgets(buffer, buffersize, fd);
+	}
+
+	// dumpster
+	free(buffer);
+	buffer = NULL;
 	fclose(fd);
 	fd = NULL;
-	// TODO: free and nullify buffer
-	return 0;
+	return totalsize;
 }
 // file: length of longest line
 int flenLongestLine(FILE *fd)
 {
 	int count = 0, max = 0;
 	char ch;
+
+	// TODO: edgecases
 
 	if (fd == NULL)
 		return -1;
@@ -115,4 +126,82 @@ int flenLongestLine(FILE *fd)
 
 	return max + 1;
 }
+
+
+// TODO implement: parse matrix, double spaces...
+int parseline(char const *line)
+{
+	// var decleration
+	int i, j, typesize[] = {4, 8, 8, 4, 2, 1, 4, 4}, totalsize = 0, currentsize;
+	// ! move types and typesize to  memoryReport!!!!!!!
+	char *types[8] = {"unsigned int",
+					  "long long",
+					  "double",
+					  "float",
+					  "short",
+					  "char",
+					  "long",
+					  "int"};
+	struct
+	{
+		int typeSize;
+		int count;
+		int isptr;
+	} data = {-1, 1, 0};
+
+	// TODO: edgecases
+
+	// body
+	for (i = 0; i < 8; i++)
+	{
+		if (strstr(line, types[i]))
+		{
+			data.typeSize = typesize[i];
+			break;
+		}
+	}
+	// TODO: edgecases for above loop
+	// ! gotta make it better
+	i = strlen(types[i]);
+	while (line[i] != '\0')
+	{
+		if (line[i] == ';')
+		{
+			currentsize = data.count * (data.isptr == 0 ? data.typeSize : 4);
+			totalsize += currentsize;
+			printf(" requires %d bytes\n", currentsize);
+			break;
+		}
+		else if (line[i] == ',')
+		{
+			currentsize = data.count * (data.isptr == 0 ? data.typeSize : 4);
+			totalsize += currentsize;
+			printf(" requires %d bytes\n", currentsize);
+			data.count = 1;
+			data.isptr = 0;
+		}
+		else if (line[i] == '*')
+		{
+			data.isptr = 1;
+		}
+		else if (line[i] == '[')
+		{
+			j = i + 1;
+			data.count = line[j++] - '0';
+			while (line[j] != ']')
+			{
+				data.count = (data.count * 10) + (line[j] - '0');
+				j++;
+			}
+			i += j - i;
+		}
+		else if(isalnum(line[i])) //if (line[i] != ']')
+		{
+			putchar(line[i]);
+		}
+		i++;
+	}
+	return totalsize;
+}
+
 // --------------------------- //
